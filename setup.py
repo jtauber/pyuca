67cf82b906add7df576138873668b96e6b83db22
James Tauber <jtauber@jtauber.com>
2012-06-21 09:59:08 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "pyuca",
+    version = "0.2",
+    description = "a Python implementation of the Unicode Collation Algorithm",
+    url = "http://github.com/jtauber/pyuca",
+    author = "James Tauber",
+    author_email = "jtauber@jtauber.com",
+    py_modules = ["pyuca"],
+)
