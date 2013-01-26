4ec394f54074567e01f71206f5464527c3daa618
James Tauber <jtauber@jtauber.com>
2013-01-25 21:04:52 -0500

    relabeled as 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "pyuca",
-    version = "0.2",
+    version = "0.3",
     description = "a Python implementation of the Unicode Collation Algorithm",
     license = "MIT",
     url = "http://github.com/jtauber/pyuca",
