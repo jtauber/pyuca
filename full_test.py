3f363b7cc93890c1c47ca690dbf725ea0dd1030a
James Tauber <jtauber@jtauber.com>
2014-02-01 17:40:44 +0800

    added full_test to CI

--- full_test.py
+++ full_test.py
@@ -1,7 +1,15 @@
-#!/usr/bin/env python3
+from __future__ import print_function
+
+import sys
 
 from pyuca import Collator
 from pyuca.utils import display_sort_key
+
+try:
+    unichr
+except NameError:
+    unichr = chr
+
 
 c = Collator()
 
@@ -12,21 +20,26 @@ failure = 0
 
 with open("CollationTest/CollationTest_NON_IGNORABLE.txt") as f:
     for line in f.readlines():
-        print()
-        print(line.strip())
         points = line.split("#")[0].split(";")[0].strip().split()
         if points:
-            test_string = "".join(chr(int(point, 16)) for point in points)
+            try:
+                test_string = "".join(
+                    unichr(int(point, 16)) for point in points
+                )
+            except ValueError:  # Python 2 narrow builds will get this
+                continue  # so just skip
+
             test_string_sort_key = c.sort_key(test_string)
             x = display_sort_key(test_string_sort_key)
-            print("###", x)
             if prev_sort_key:
                 if prev_sort_key > test_string_sort_key:
                     failure += 1
-                    break
                 else:
                     success += 1
             prev_sort_key = test_string_sort_key
 
 print()
 print("{} success; {} failure".format(success, failure))
+
+if failure > 0:
+    sys.exit(1)
