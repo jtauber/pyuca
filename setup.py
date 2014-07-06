01b917e7a16fcbf7db2b927ac5e696e040ba4f78
James Tauber <jtauber@jtauber.com>
2014-07-06 14:31:23 -0400

    added 3.4 to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Text Processing",
     ],
 )
