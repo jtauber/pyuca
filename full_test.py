35082804cfd964c5d6e9209f012980fc643212e9
Chris Beaven <smileychris@gmail.com>
2014-10-20 23:03:47 +1300

    Finish Python 2 integration with working tests

--- full_test.py
+++ full_test.py
@@ -4,7 +4,6 @@ import sys
 from io import open
 
 from pyuca import Collator
-from pyuca.collator import UCA_VERSION
 from pyuca.utils import format_sort_key
 
 try:
@@ -19,7 +18,8 @@ prev_sort_key = None
 success = 0
 failure = 0
 
-path = "CollationTest/{0}/CollationTest_NON_IGNORABLE.txt".format(UCA_VERSION)
+path = "CollationTest/{0}/CollationTest_NON_IGNORABLE.txt".format(
+    c.UCA_VERSION)
 
 with open(path) as f:
     for i, line in enumerate(f.readlines()):
@@ -30,21 +30,23 @@ with open(path) as f:
                 chr(int(point, 16)) for point in points
             )
             test_string_sort_key = c.sort_key(test_string)
-            x = format_sort_key(test_string_sort_key)
             if prev_sort_key:
                 if prev_sort_key > test_string_sort_key:
                     failure += 1
-                    print(i)
-                    print(line)
-                    print(x)
+                    print('-------')
+                    print("failed on line {0}:".format(i+1))
+                    print(line.rstrip('\n'))
+                    print("PREV: {0}".format(format_sort_key(prev_sort_key)))
+                    print("THIS: {0}".format(
+                        format_sort_key(test_string_sort_key)))
                     print('-------')
                 else:
                     success += 1
             prev_sort_key = test_string_sort_key
 
-print()
+print("")
 print("{0} success; {1} failure (UCA version {2})".format(
-    success, failure, UCA_VERSION))
+    success, failure, c.UCA_VERSION))
 
 if failure > 0:
     sys.exit(1)
