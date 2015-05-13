fd012a088829010149511196988d9500785186cc
Feanil Patel <feanil@edx.org>
2015-05-13 11:14:20 -0400

    Update setup.py to a new version number
    @jtauber so that you can push a new version to PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyuca",
-    version="1.0",
+    version="1.1",
     description="a Python implementation of the Unicode Collation Algorithm",
     license="MIT",
     url="http://github.com/jtauber/pyuca",
