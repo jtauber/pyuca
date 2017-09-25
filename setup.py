d751d694a90ab9d8789263822e02ae822d251e2c
James Tauber <jtauber@jtauber.com>
2017-09-25 19:05:32 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup
 
 setup(
     name="pyuca",
-    version="1.1.2",
+    version="1.2",
     description="a Python implementation of the Unicode Collation Algorithm",
     license="MIT",
     url="http://github.com/jtauber/pyuca",
     author="James Tauber",
     author_email="jtauber@jtauber.com",
     packages=["pyuca"],
-    package_data={"": ["allkeys-5.2.0.txt", "allkeys-6.3.0.txt"]},
+    package_data={"": [
+        "allkeys-5.2.0.txt",
+        "allkeys-6.3.0.txt",
+        "allkeys-8.0.0.txt",
+        "allkeys-9.0.0.txt",
+        "allkeys-10.0.0.txt",
+    ]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
@@ -17,6 +23,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Text Processing",
     ],
 )
